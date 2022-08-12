5bc431b59c5e006a587767e5f2e2e381396b32a2
HadiJafari <haadijafari@yahoo.com>
2022-08-12 21:44:49 +0430

    [Comments added]: Python ==> Comments for python answer were added

--- 14.LongestCommonPrefix/LongestCommonPrefix.py
+++ 14.LongestCommonPrefix/LongestCommonPrefix.py
@@ -1,13 +1,21 @@
 class Solution:
     def longestCommonPrefix(self, strs: list[str]) -> str:
+        # a string to save confirmed prefix string
         result = ''
         
+        #loop through every charecter of first string
         for i in range(len(strs[0])):
+            #loop through every string in the list
             for s in strs:
+                #checks if the index is out of range or not
+                #then checks if the chareckter is the same or not
                 if i >= len(s) or strs[0][i] != s[i]:
+                    #returns the result if the statement is true
                     return result
+            #adds the charecter if everything is fine
             result += strs[0][i]
-        
+            
+        #returns the result if the charecters are all checked
         return result
     
 
