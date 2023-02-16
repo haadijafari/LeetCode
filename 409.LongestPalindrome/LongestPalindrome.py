cb2b7e9e26a7b420004546ab602f4c9941591b43
Hadi Jafari <sig.haadijafari@gmail.com>
2023-02-16 23:26:40 +0330

    409.LongestPalindrome_FirstAnswer
    - counts characters, every even counts can be added without condition but the odd ones are added till they are not greater than one. at the end one odd can be added at the center.

---
+++ 409.LongestPalindrome/LongestPalindrome.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        c = Counter(s)
+        output = 0
+        for count in c.values():
+            output += int(count/2) * 2
+            if output % 2 == 0 and count % 2 == 1:
+                output += 1
+        return output
